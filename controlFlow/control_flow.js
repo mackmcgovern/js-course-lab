58bda228298423bebfa49204ea8f1f8c0abf5ad2
mackmcgovern <mkrmcgovern@gmail.com>
2025-02-05 03:15:43 -0500

    added final practice exercise

--- controlFlow/control_flow.js
+++ controlFlow/control_flow.js
@@ -1,26 +1,26 @@
-let userRole = "admin";
-let accessLevel;
+// let userRole = "admin";
+// let accessLevel;
 
-if (userRole === "admin") {
-    accessLevel = "Full access granted";
-} else if (userRole === "manager") {
-    accessLevel = "Limited access granted";
-} else {
-    accessLevel = "No access granted";
-}
+// if (userRole === "admin") {
+//     accessLevel = "Full access granted";
+// } else if (userRole === "manager") {
+//     accessLevel = "Limited access granted";
+// } else {
+//     accessLevel = "No access granted";
+// }
 
-console.log("Access Level:", accessLevel);
+// console.log("Access Level:", accessLevel);
 
-let isLoggedIn = true;
-let userMessage;
+// let isLoggedIn = true;
+// let userMessage;
 
-if (isLoggedIn) {
-  userMessage = userRole === "admin" ? "Welcome, Admin!" : "Welcome, User!";
-} else {
-    userMessage = "Please login to access the system."
-}
+// if (isLoggedIn) {
+//   userMessage = userRole === "admin" ? "Welcome, Admin!" : "Welcome, User!";
+// } else {
+//     userMessage = "Please login to access the system."
+// }
 
-console.log(userMessage)
+// console.log(userMessage)
 
 let userType = "subscriber";
 let userCategory;
@@ -44,3 +44,25 @@ console.log(`User Category: ${userCategory}`);
 let isAuthenticated = true;
 let authenticationSatus = isAuthenticated ? "Authenticated" : "Not Authenticated"
 console.log(`Authentication Status: ${authenticationSatus}`);
+
+
+// Practice Task 
+let userRole = 'Subscriber'
+let serviceAccess;
+
+switch(userRole) {
+    case "Employee":
+        serviceAccess = "Dietary Services"
+        break;
+    case "Enrolled Member":
+        serviceAccess = "Dietary Services and 1:1 interaction with a dietician."
+        break;
+    case "Subscriber":
+        serviceAccess = "Partial access to facilitate dietary services."
+        break;
+    default:
+        serviceAccess = "No access to dietary services unless enrolled or subscribed."
+        break;
+}
+
+console.log(`Access to: ${serviceAccess}`);
